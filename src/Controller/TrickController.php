4247a2eb7b8b0e3171cd289bcbe99eba89e4fcb8
Yokke <mdembelepro@gmail.com>
2023-09-15 20:02:39 +0200

    refactor: symfony form use for update trick

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -8,6 +8,10 @@ use App\Repository\CommentRepository;
 use App\Repository\MediaRepository;
 use App\Repository\TrickRepository;
 use App\Repository\UserRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use IntlDateFormatter;
@@ -15,15 +19,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Trick;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TrickController extends AbstractController
 {
-    public string $template = "";
-    /**
-     * @var array<string,int>
-     */
-    public array $parameters = [];
+
+    private Trick $trick;
+    private IntlDateFormatter $dateFormatter;
+
+    public function __construct()
+    {
+        $this->trick = new Trick();
+        $this->dateFormatter = new IntlDateFormatter('fr_Fr', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+    }
 
     #[Route('/{trickname}/details/{id}', name: 'trick', methods: ["GET"])]
     public function getTrickPage(
@@ -42,16 +51,16 @@ class TrickController extends AbstractController
         $medias = $mediaRepository->getTrickMedia($id);
         $trickComments = $commentRepository->getComments($id, $userRepository);
 
-        if($request->query->get('page') !== null && !empty($request->query->get('page'))){
+        if ($request->query->get('page') !== null && !empty($request->query->get('page'))) {
             $currentPage = $request->query->get('page');
-        } else{
+        } else {
             $currentPage = 1;
         }
         $nbComments = count($trickComments);
         $commentsPerPage = 10;
-        $pages = ceil($nbComments/$commentsPerPage);
+        $pages = ceil($nbComments / $commentsPerPage);
         $firstPage = ($currentPage * $commentsPerPage) - $commentsPerPage;
-        $commentsPerPageRequest = $commentRepository->getCommentsPerPage($firstPage,$commentsPerPage);
+        $commentsPerPageRequest = $commentRepository->getCommentsPerPage($firstPage, $commentsPerPage);
         $parameters["comments"] = $commentsPerPageRequest;
         $parameters["pages"] = $pages;
         $parameters["currentPage"] = $currentPage;
@@ -65,12 +74,10 @@ class TrickController extends AbstractController
         $parameters["trick_date"] = $date;
 
 
-
         $parameters["user_connected"] = !empty($userConnected) ? $userConnected : '';
 
         return new Response($this->render($template, $parameters));
     }
-
 
 
     #[Route('/create-trick', name: 'create_trick_get', methods: ["GET"])]
@@ -162,15 +169,56 @@ class TrickController extends AbstractController
     ): Response {
         $template = "update_trick.twig";
         $userConnected = $request->getSession()->get('user_connected');
-        $trick = $trickRepository->getTrick($id);
+        $trick = current($trickRepository->findBy(["id" => $id]));
+        $formBuilder = $this->createFormBuilder()
+            ->add("nameupdated", TextType::class, options: [
+                'label' => 'Nom du trick',
+                'required' => false,
+                'constraints' => [
+                    new Regex(
+                        pattern: "/^[A-ZÀ-ÿ][A-Za-zÀ-ÿ, .'\-\n]*$/u",
+                        message: 'Oops! Le format de votre saisie est incorrect, le nom du trick doit commencer par une lettre majuscule',
+                        match: true,
+                    )
+                ],
+                "attr" => ["value" => $trick->getName()]
+            ])
+            ->add('description', TextareaType::class, options: [
+                'label' => 'Description',
+                'required' => false,
+                'constraints' =>
+                    [
+                        new Regex(
+                            pattern: "/^[A-ZÀ-ÿ][A-Za-zÀ-ÿ, .'\-\n]*$/u",
+                            message: 'Oops! Le format de votre saisie est incorrect, votre description doit commencer par une lettre majuscule',
+                            match: true,
+                        )
+                    ],
+                "data" => $trick->getDescription(),
+            ])
+            ->add('trickGroup', ChoiceType::class, options: [
+                'label' => 'Sélectionner un groupe',
+                "choices" => [
+                    $trick->getTrickGroup() => true,
+                    "Grabs" => "Grabs",
+                    "Rotations" => "Rotations",
+                    "Flips" => "Flips",
+                    "Rotation désaxées" => "Rotation désaxées",
+                    "Slides" => "Slides",
+                    "One Foot Tricks" => "One Foot Tricks",
+                    "Old School" => "Old School"
+                ]
+            ])->add('save', SubmitType::class, ['label' => 'Envoyer', 'attr' => ['class' => 'btn btn-dark']]
+            )->setMethod("PUT");
+        $form = $formBuilder->getForm();
         $medias = $mediaRepository->getTrickMedia($id);
         $trick->setName(str_replace('-', ' ', ucfirst($trickname)));
-        $frenchDateFormat = new IntlDateFormatter('fr_Fr', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
         $dateTrick = $trick->getDate();
-        $date = $frenchDateFormat->format($dateTrick);
+        $date = $this->dateFormatter->format($dateTrick);
         $parameters["trick"] = $trick;
         $parameters["medias"] = $medias;
         $parameters["trick_date"] = $date;
+        $parameters["form"] = $form;
         $parameters["user_connected"] = !empty($userConnected) ? $userConnected : '';
         return new Response($this->render($template, $parameters));
     }
@@ -263,46 +311,72 @@ class TrickController extends AbstractController
         int $id,
         string $trickname,
         Request $request,
-        ValidatorInterface $validator,
         TrickRepository $trickRepository,
         MediaRepository $mediaRepository
     ): Response {
         $template = "update_trick.twig";
-        $numberOfErrors = 0;
-        $trick = $trickRepository->getTrick($id);
-        $media = $mediaRepository->getTrickMedia($id);
-        $trickEntity = new Trick();
-        $trickEntity->setNameUpdated($request->request->get('trick-name'));
-        $trickEntity->setDescription($request->request->get('description'));
-        $trickEntity->setTrickGroup($request->request->get('selected_options'));
-        $groups = [
-            "namex_exception",
-            "description_exception",
-            "group_exception",
-        ];
-        $groupsViolations = [];
-        foreach ($groups as $group) {
-            $errors = $validator->validate($trickEntity, null, $group);
-            if (count($errors) >= 1) {
-                $numberOfErrors++;
-            }
-            foreach ($errors as $error) {
-                $groupsViolations[$group] = $error->getMessage();
-            }
-        }
+        $trick = current($trickRepository->findBy(["id" => $id]));
+        $media = $mediaRepository->findBy(["idTrick" => $id]);
 
-        if ($numberOfErrors == 0) {
-            $trickUpdated = $trickRepository->updateTrick($id, $trickEntity);
-            if ($trickUpdated) {
+        $formBuilder = $this->createFormBuilder()
+            ->add("nameupdated", TextType::class, options: [
+                'label' => 'Nom du trick',
+                'required' => false,
+                'constraints' => [
+                    new Regex(
+                        pattern: "/^[A-ZÀ-ÿ][A-Za-zÀ-ÿ, .'\-\n]*$/u",
+                        message: 'Oops! Le format de votre saisie est incorrect, le nom du trick doit commencer par une lettre majuscule',
+                        match: true,
+                    )
+                ],
+                "attr" => ["value" => $trick->getName()]
+            ])
+            ->add('description', TextareaType::class, options: [
+                'label' => 'Description',
+                'required' => false,
+                'constraints' =>
+                    [
+                        new Regex(
+                            pattern: "/^[A-ZÀ-ÿ][A-Za-zÀ-ÿ, .'\-\n]*$/u",
+                            message: 'Oops! Le format de votre saisie est incorrect, votre description doit commencer par une lettre majuscule',
+                            match: true,
+                        )
+                    ],
+                "data" => $trick->getDescription(),
+            ])
+            ->add('trickGroup', ChoiceType::class, options: [
+                'label' => 'Sélectionner un groupe',
+                "choices" => [
+                    $trick->getTrickGroup() => true,
+                    "Grabs" => "Grabs",
+                    "Rotations" => "Rotations",
+                    "Flips" => "Flips",
+                    "Rotation désaxées" => "Rotation désaxées",
+                    "Slides" => "Slides",
+                    "One Foot Tricks" => "One Foot Tricks",
+                    "Old School" => "Old School"
+                ]
+            ])->add('save', SubmitType::class, ['label' => 'Envoyer', 'attr' => ['class' => 'btn btn-dark']]
+            )->setMethod("PUT");
+        $form = $formBuilder->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid() && $form->isSubmitted()) {
+            $token = $request->request->all()["form"]["_token"];
+            if ($this->isCsrfTokenValid("form", $token)) {
+                $this->trick->setNameUpdated($form->getData()["nameupdated"]);
+                $this->trick->setDescription($form->getData()["description"]);
+                $this->trick->setTrickGroup($form->getData()["trickGroup"]);
+                $trickRepository->updateTrick($id, $this->trick);
                 $this->addFlash("success", "Votre fichier a bien été mis à jour !");
                 return $this->redirectToRoute('homepage');
             }
         }
+
         $userConnected = $request->getSession()->get('user_connected');
         $parameters["user_connected"] = !empty($userConnected) ? $userConnected : '';
         $parameters["medias"] = $media;
+        $parameters["form"] = $form;
         $parameters["trick"] = $trick;
-        $parameters["exceptions"] = $groupsViolations;
         return new Response($this->render($template, $parameters), 400);
     }
 
@@ -337,10 +411,8 @@ class TrickController extends AbstractController
     public function deleteTrickMedia(int $id, MediaRepository $mediaRepository): Response
     {
         $media = current($mediaRepository->findBy(["id" => $id]));
-        if($media->getMediaType() != "web")
-        {
+        if ($media->getMediaType() != "web") {
             unlink("../public" . $media->getMediaPath());
-
         }
         $mediaRepository->getEntityManager()->remove($media);
         $mediaRepository->getEntityManager()->flush();
