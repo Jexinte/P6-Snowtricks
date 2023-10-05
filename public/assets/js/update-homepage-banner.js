fcc434102b44c0ff9093246b244d148a32084199
Yokke <mdembelepro@gmail.com>
2023-10-05 20:54:42 +0200

    update

---
+++ public/assets/js/update-homepage-banner.js
@@ -0,0 +1,11 @@
+const bannerImg = document.querySelector('#banner img')
+const bannerImgDefaultPath = document.querySelector('#banner img').src
+window.addEventListener('resize',(e) => {
+    if(window.innerWidth <= 992)
+    {
+        bannerImg.src = "/assets/img/banner/banner-mobile.jpg"
+    } else {
+        bannerImg.src = bannerImgDefaultPath
+    }
+})
+
